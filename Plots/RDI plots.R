5574af78ae3ddb149dbaf2e5756092cccdea9362
ianhussey <Ian@Ians-MacBook-Air.local>
2016-11-07 22:42:22 +0100

    text changes to RDI plots

--- Plots/RDI plots.R
+++ Plots/RDI plots.R
@@ -18,11 +18,11 @@ setwd("/Users/Ian/Dropbox/Work/Manuscripts/Hussey & De Houwer, Learning via IAT 
 
 exp1_df <- 
   read.csv("/Users/Ian/Dropbox/Work/Manuscripts/Hussey & De Houwer, Learning via IAT CC/Analogical learning via IAT - OSF files/Experiment 1/Analyses/dataset.csv") %>%
-  mutate(Condition = ifelse(condition == "flowers", "Flowers", "Insects"))
+  mutate(`Experiment 1` = ifelse(condition == "flowers", "Flowers", "Insects"))
 
 exp2_df <- 
   read.csv("/Users/Ian/Dropbox/Work/Manuscripts/Hussey & De Houwer, Learning via IAT CC/Analogical learning via IAT - OSF files/Experiment 2/Analyses/dataset.csv") %>%
-  mutate(Condition = ifelse(condition == "flowers", "Flowers", "Insects"))
+  mutate(`Experiment 2` = ifelse(condition == "flowers", "Flowers", "Insects"))
 
 
 ###################################################################
@@ -32,9 +32,9 @@ exp2_df <-
 par(mfrow=c(1,3))
 
 RDI_plot_exp1_ratings <- 
-  pirateplot(formula = ratings_change_scores ~ Condition,
+  pirateplot(formula = ratings_change_scores ~ `Experiment 1`,
              data = exp1_df,
-             ylab = "Exp 1: Ratings change scores",
+             ylab = "Ratings",
              theme.o = 1,
              pal = "526273",
              bean.o = 1,
@@ -46,9 +46,9 @@ RDI_plot_exp1_ratings <-
              jitter.val = 0.05)  # confidence intervals
 
 RDI_plot_exp2_ratings <- 
-  pirateplot(formula = ratings_change_scores ~ Condition,
+  pirateplot(formula = ratings_change_scores ~ `Experiment 2`,
              data = exp2_df,
-             ylab = "Exp 2: Ratings change scores",
+             ylab = "Ratings",
              theme.o = 1,
              pal = "526273",
              bean.o = 1,
@@ -60,9 +60,9 @@ RDI_plot_exp2_ratings <-
              jitter.val = 0.05)  # confidence intervals
 
 RDI_plot_exp2_SCIATs <- 
-  pirateplot(formula = SCIAT_D1_change_scores ~ Condition,
+  pirateplot(formula = SCIAT_D1_change_scores ~ `Experiment 2`,
              data = exp2_df,
-             ylab = substitute(paste("Exp 2: ", italic("D"), "1 change scores")),
+             ylab = substitute(paste("SC-IAT ", italic("D"), "1")),
              theme.o = 1,
              pal = "526273",
              bean.o = 1,
